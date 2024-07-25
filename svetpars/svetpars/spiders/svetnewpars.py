46ad606bd1787981fdd6efbbdb023d40678a57b8
DimarashkA <159241659+DimarashkA@users.noreply.github.com>
2024-07-25 10:02:31 +0300

    add new

---
+++ svetpars/svetpars/spiders/svetnewpars.py
@@ -0,0 +1,16 @@
+import scrapy
+
+
+class SvetnewparsSpider(scrapy.Spider):
+    name = "svetnewpars"
+    allowed_domains = ["https://divan.ru"]
+    start_urls = ["https://www.divan.ru/category/svet"]
+
+    def parse(self, response):
+        svets = response.css('div.LlPhv')
+        for svet in svets:
+            yield {
+                'name' : svet.css('div.wYUX2 span::text').get(),
+                'price' : svet.css('div.ui-LD-ZU KIkOH span::text').get(),
+                'url' : divan.css('a').attrib['href']
+            }
