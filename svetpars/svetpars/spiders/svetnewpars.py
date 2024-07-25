ef6572d23dcdf005c5bd36e50fa1f00786d0a206
DimarashkA <159241659+DimarashkA@users.noreply.github.com>
2024-07-25 12:32:44 +0300

    final

--- svetpars/svetpars/spiders/svetnewpars.py
+++ svetpars/svetpars/spiders/svetnewpars.py
@@ -4,13 +4,13 @@ import scrapy
 class SvetnewparsSpider(scrapy.Spider):
     name = "svetnewpars"
     allowed_domains = ["https://divan.ru"]
-    start_urls = ["https://www.divan.ru/category/svet"]
+    start_urls = ["https://www.divan.ru/category/svet/page-2"]
 
     def parse(self, response):
         svets = response.css('div.WdR1o')
         for svet in svets:
             yield {
                 'name' : svet.css('div.wYUX2 span::text').get(),
-                'price' : svet.css('div.ui-LD-ZU KIkOH span::text').get(),
+                'price' : svet.css('div.q5Uds span::text').get(),
                 'url' : svet.css('a').attrib['href']
             }
